87ab28a87065a27baf4c7475fd827cb2ef33a304
Jerry Reinoehl <jerryreinoehl@gmail.com>
2021-10-21 19:26:00 -0500

    Use PROMPT_COLOR array to set colors of prompt components

--- prompt.zsh
+++ prompt.zsh
@@ -3,6 +3,14 @@
 # v0.1.0
 # ============================================================================
 
+declare -A PROMPT_COLOR
+PROMPT_COLOR[host]="1;32"
+PROMPT_COLOR[dir]="1;34"
+PROMPT_COLOR[ptr]="1"
+PROMPT_COLOR[error]="1;31"
+PROMPT_COLOR[bgjobs]="1;2;37"
+PROMPT_COLOR[branch]="1;35"
+
 precmd_functions+=(__prompt)
 
 __prompt() {
@@ -16,7 +24,9 @@ __prompt() {
     __prompt_bgjobs; bgjobs="$REPLY"
     __prompt_branch; branch="$REPLY"
 
-    PS1="%B%F{green}%n@%m%f %F{blue}%1~%f%b $ptr "
+    PS1=$'%{\e['$PROMPT_COLOR[host]$'m%}%n@%m%{\e[0m%}'
+    PS1+=$' %{\e['$PROMPT_COLOR[dir]$'m%}%1~%{\e[0m%}'
+    PS1+=" $ptr "
 
     RPS1=""
     [[ -n "$error" ]] && RPS1+="$error"
@@ -26,20 +36,20 @@ __prompt() {
 
 __prompt_pointer() {
     [[ "$__pipestatus" =~ ^0( 0)*$ ]] \
-        && REPLY="%B%(!.#.>)%b" \
-        || REPLY="%B%F{red}%(!.#.>)%f%b"
+        && REPLY=$'%{\e['$PROMPT_COLOR[ptr]$'m%}%(!.#.>)%{\e[0m%}' \
+        || REPLY=$'%{\e['$PROMPT_COLOR[error]$'m%}%(!.#.>)%{\e[0m%}'
 }
 
 __prompt_error() {
     [[ "$__pipestatus" =~ ^0( 0)*$ ]] \
         && REPLY="" \
-        || REPLY="%B%F{red}[$__pipestatus]%f%b"
+        || REPLY=$'%{\e['$PROMPT_COLOR[error]'m%}['$__pipestatus$']%{\e[0m%}'
 }
 
 __prompt_bgjobs() {
     local -i num_jobs=${#jobtexts[@]}
     (( num_jobs > 0 )) \
-        && REPLY=$'%{\e[1;2;37m%}*'$num_jobs$'%{\e[0m%}' \
+        && REPLY=$'%{\e['$PROMPT_COLOR[bgjobs]'m%}*'$num_jobs$'%{\e[0m%}' \
         || REPLY=""
 }
 
@@ -52,7 +62,7 @@ __prompt_branch() {
     __prompt_git_branch "$head"; branch="$REPLY"
 
     [[ -z "$branch" ]] && REPLY="" && return
-    REPLY=$'%B%F{magenta}\u2387 '$branch'%f%b'
+    REPLY=$'%{\e['$PROMPT_COLOR[branch]$'m%}\u2387 '$branch$'%{\e[0m%}'
 }
 
 __prompt_git_head() {
